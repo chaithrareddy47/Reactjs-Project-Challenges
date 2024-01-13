fc364156f426ee09e22a65233560d0d80e4ff7c1
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2024-01-13 21:23:36 +0530

    Add files via upload

---
+++ 01-Form/src/constants/config.js
@@ -0,0 +1,2 @@
+export const IMG_URL =
+	'https://images.unsplash.com/photo-1515041219749-89347f83291a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FydG9vbnxlbnwwfHwwfHx8MA%3D%3D';
---
+++ 01-Form/src/constants/mockData.js
@@ -0,0 +1,28 @@
+const greetingsData = [
+	{
+		name: 'Alice',
+		message: 'Hello, Alice! Have a great day.',
+		imageUrl: `https://plus.unsplash.com/premium_photo-1682308336208-7f3c19e6a96b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FydG9vbnxlbnwwfHwwfHx8MA%3D%3D`,
+	},
+	{
+		name: 'Bob',
+		message: 'Hey Bob, wishing you a wonderful day ahead.',
+		imageUrl: `https://images.unsplash.com/photo-1639628735078-ed2f038a193e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+	},
+	{
+		name: 'Charlie',
+		message: `Hi Charlie! Hope you're doing well.`,
+		imageUrl: `https://plus.unsplash.com/premium_photo-1661627453805-ee98257b27cc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGNhcnRvb258ZW58MHx8MHx8fDA%3D`,
+	},
+	{
+		name: 'Daisy',
+		message: 'Good morning, Daisy! Enjoy your day.',
+		imageUrl: `https://images.unsplash.com/photo-1633069404479-2a8aa9526ea6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGNhcnRvb258ZW58MHx8MHx8fDA%3D`,
+	},
+	{
+		name: 'Ella',
+		message: 'Hi Ella! Sending positive vibes your way.',
+		imageUrl: `https://images.unsplash.com/photo-1630207831419-3532bcb828d7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fGNhcnRvb258ZW58MHx8MHx8fDA%3D`,
+	},
+];
+export default greetingsData;
